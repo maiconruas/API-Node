8cafa9d8f341fa85e9be2e35d1fcfa4678ed3369
Maicon Ruas <maicon_ruas@hotmail.com>
2023-05-22 10:51:48 -0300

    Criando exemplo do CRUD

--- src/app.js
+++ src/app.js
@@ -1,15 +1,38 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
 
-const route = router.get('/', (req, res, next) => {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+const read = router.get('/', (req, res, next) => {
     res.status(200).send({
         title: "Node Store API",
         version: "0.0.1"
     });
 });
 
-app.use('/', route);
+const create = router.post('/', (req, res, next) => {
+    res.status(201).send(req.body);
+});
+
+const update = router.put('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(200).send({
+        id: id,
+        item: req.body
+    });
+});
+
+const deletar = router.post('/:id', (req, res, next) => {
+    res.status(200).send(req.body);
+});
+
+app.use('/', read);
+app.use('/products', create);
+app.use('/products', update);
+app.use('/products', deletar);
 
 module.exports = app;
