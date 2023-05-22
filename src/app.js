ea2774c0f0e41a7d8e56fb900e4152562b528d22
Maicon Ruas <maicon_ruas@hotmail.com>
2023-05-22 12:38:13 -0300

    Conexão com base de dados

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
+
+// Connecta ao banco
+mongoose.connect('Connect String');
+
+// Carregar Models
+const Product = require('./models/product');
 
 //Carregar as rotas
 const index = require('./routes/index-route');
@@ -10,8 +17,6 @@ const product = require('./routes/product-route');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
-
-
 
 app.use('/', index);
 app.use('/products', product);
