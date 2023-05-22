f02e9511675c1d7f82d407285bd30f1565154c5a
Maicon Ruas <maicon_ruas@hotmail.com>
2023-05-22 12:51:52 -0300

    Criando o controller para listar os Produtos atraves do Slug

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -4,7 +4,19 @@ const Product = mongoose.model('Product');
 exports.get = (req, res, next) => {
     Product.find({ active: true }, 'title price slug')
         .then(data => {
-            res.status(201).send(data);
+            res.status(200).send(data);
+        }).catch(e =>{
+             res.status(400).send(e);
+        });
+};
+
+exports.getBySlug = (req, res, next) => {
+    Product.findOne({
+        slug: req.params.slug,
+        active: true
+        }, 'title price slug tags')
+        .then(data => {
+            res.status(200).send(data);
         }).catch(e =>{
              res.status(400).send(e);
         });
