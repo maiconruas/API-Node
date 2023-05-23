0fab2e06cf87c55786e3ad0c8897c952fc2704bc
Maicon Ruas <maicon_ruas@hotmail.com>
2023-05-23 12:35:18 -0300

    Criando o Controller, Repository e a Rota da Order

--- src/app.js
+++ src/app.js
@@ -17,12 +17,14 @@ const Order = require('./models/order');
 const index = require('./routes/index-route');
 const product = require('./routes/product-route');
 const customer = require('./routes/customer-route');
+const order = require('./routes/order-route');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
 app.use('/', index);
 app.use('/products', product);
-app.use('/customer', customer);
+app.use('/customers', customer);
+app.use('/orders', order);
 
 module.exports = app;
