1affb84a336aac39ae814ae78254f9da392185f5
Maicon Ruas <maicon_ruas@hotmail.com>
2023-05-23 12:26:22 -0300

    Apagando lixo no controller

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -51,31 +51,12 @@ exports.post = async(req, res, next) => {
     contract.hasMinLen(req.body.slug, 3, 'O título deve conter pelo menos 3 caracteres');
     contract.hasMinLen(req.body.description, 3, 'O título deve conter pelo menos 3 caracteres');
 
-    // Se os dados forem inválidos
     if (!contract.isValid()) {
         res.status(400).send(contract.errors()).end();
         return;
     }
 
     try {
-        // Cria o Blob Service
-        // const blobSvc = azure.createBlobService(config.containerConnectionString);
-//
-//        let filename = guid.raw().toString() + '.jpg';
-//        let rawdata = req.body.image;
-//        let matches = rawdata.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
-//        let type = matches[1];
-//        let buffer = new Buffer(matches[2], 'base64');
-//
-//        // Salva a imagem
-//        await blobSvc.createBlockBlobFromText('product-images', filename, buffer, {
-//            contentType: type
-//        }, function (error, result, response) {
-//            if (error) {
-//                filename = 'default-product.png'
-//            }
-//        });
-
         await repository.create({
             title: req.body.title,
             slug: req.body.slug,
@@ -83,7 +64,6 @@ exports.post = async(req, res, next) => {
             price: req.body.price,
             active: true,
             tags: req.body.tags,
-//            image: 'https://nodestr.blob.core.windows.net/product-images/' + filename
         });
         res.status(201).send({
             message: 'Produto cadastrado com sucesso!'
